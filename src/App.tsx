0f3df2ee2fe80028920bcd37fb6d353fed3e582a
smqz <33373071+smqz@users.noreply.github.com>
2017-11-30 14:22:27 +0400

    Changed to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code>.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component<any, any> {
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<h2>Welcome to React</h2>
+				</div>
+				<p className="App-intro">
+					Example App
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
